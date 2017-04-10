aa8ee4a08e8b997c7402f127f3c50531ce74d9f9
maximebeckers <ma.beckers@student.helmo.be>
2017-04-10 11:35:58 +0200

    creation interfaces vides

--- app/src/main/java/com/example/benja/todolist_mathy_beckers/view/MainActivity.java
+++ app/src/main/java/com/example/benja/todolist_mathy_beckers/view/MainActivity.java
@@ -1,14 +1,15 @@
-package com.example.benja.todolist_mathy_beckers;
+package com.example.benja.todolist_mathy_beckers.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.benja.todolist_mathy_beckers.R;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_main_alllist);
     }
 }
