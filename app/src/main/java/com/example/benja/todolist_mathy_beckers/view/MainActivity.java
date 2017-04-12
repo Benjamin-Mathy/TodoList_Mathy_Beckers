bc390a8ffc562c5624bf0184f4ba6ce92788cc38
maximebeckers <ma.beckers@student.helmo.be>
2017-04-12 17:40:32 +0200

    menu et couleurs de base

--- app/src/main/java/com/example/benja/todolist_mathy_beckers/view/MainActivity.java
+++ app/src/main/java/com/example/benja/todolist_mathy_beckers/view/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.benja.todolist_mathy_beckers.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 import com.example.benja.todolist_mathy_beckers.R;
 
@@ -11,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_alllist);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
