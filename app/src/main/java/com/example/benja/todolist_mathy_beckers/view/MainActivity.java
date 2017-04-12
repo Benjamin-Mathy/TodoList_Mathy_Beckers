4ba104769ca88698f7d47530ab17ae1823a8bb79
maximebeckers <ma.beckers@student.helmo.be>
2017-04-12 17:50:51 +0200

    not much

--- app/src/main/java/com/example/benja/todolist_mathy_beckers/view/MainActivity.java
+++ app/src/main/java/com/example/benja/todolist_mathy_beckers/view/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.benja.todolist_mathy_beckers.view;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -16,7 +17,8 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
