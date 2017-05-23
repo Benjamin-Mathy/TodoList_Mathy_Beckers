ccbacfd35c51191c4a3daac5484ea98949eeff6f
Benjamin MATHY <b.mathy@student.helmo.be>
2017-05-23 11:20:19 +0200

    delete ont TODO

--- app/src/main/java/com/example/benja/todolist_mathy_beckers/model/MapManager.java
+++ app/src/main/java/com/example/benja/todolist_mathy_beckers/model/MapManager.java
@@ -85,13 +85,6 @@ public class MapManager implements OnMapReadyCallback, LocationListener {
     public void onConnected(@Nullable Bundle bundle) {
         if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
             return;
         }
         mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
